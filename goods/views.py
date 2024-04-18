a9d078da3226d50ff5e83066c460f7a1e589d61a
Denis <suzdalev.denis@mail.ru>
2024-04-18 13:17:18 +0300

    изменения

--- goods/views.py
+++ goods/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 
 def catalog(request):
-    return render
+    return render(request, 'goods/catalog.html')
 
 
 def product(request):
-    return render
+    return render(request, 'goods/product.html')
+0
