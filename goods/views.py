e4495896d5d8dad8591115705419d6bb43785f41
Denis <suzdalev.denis@mail.ru>
2024-04-23 18:03:27 +0300

    ggg

--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_list_or_404
+from django.core.paginator import Paginator
 from goods.models import Products
 
 
@@ -9,9 +10,12 @@ def catalog(request, category_slug):
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(1)
+
     context = {
         "title": "Home - Каталог",
-        "goods": goods
+        "goods": current_page
     }
     return render(request, 'goods/catalog.html', context)
 
