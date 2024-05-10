8be715e494dbdc9e3761fdfd544ef91ec029b7ad
Denis <suzdalev.denis@mail.ru>
2024-05-10 15:45:33 +0300

    yes

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from carts.admin import CartTabAdmin
 from users.models import User
 
 # admin.site.register(User)
@@ -9,3 +10,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "first_name", "last_name", "email",]
     search_fields = ["username", "first_name", "last_name", "email",]
 
+    inlines = [CartTabAdmin,]
