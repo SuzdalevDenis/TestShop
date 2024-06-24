0ca21b5f845a5dc9eb9c14d9b72c461e1bf65e9f
Denis <suzdalev.denis@mail.ru>
2024-06-24 20:00:49 +0300

    The End soon...(added Rest_framework)

--- carts/views.py
+++ carts/views.py
@@ -2,8 +2,10 @@ from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
+from django.views import View
 from rest_framework.response import Response
 
+from carts.mixins import CartMixin
 from carts.models import Cart
 from carts.utils import get_user_carts
 # from carts.serializers import CartSerializer
@@ -12,88 +14,151 @@ from goods.models import Products
 from rest_framework import viewsets, mixins, generics
 
 
-def cart_add(request):
-    product_id = request.POST.get("product_id")
+class CartAddView(CartMixin, View):
+    def post(self, request):
+        product_id = request.POST.get("product_id")
+        product = Products.objects.get(id=product_id)
 
-    product = Products.objects.get(id=product_id)
+        cart = self.get_cart(request, product=product)
 
-    if request.user.is_authenticated:
-        carts = Cart.objects.filter(user=request.user, product=product)
-
-        if carts.exists():
-            cart = carts.first()
-            if cart:
-                cart.quantity += 1
-                cart.save()
+        if cart:
+            cart.quantity += 1
+            cart.save()
         else:
-            Cart.objects.create(user=request.user, product=product, quantity=1)
+            Cart.objects.create(user=request.user if request.user.is_authenticated else None,
+                                session_key=request.session.session_key if not request.user.is_authenticated else None,
+                                product=product, quantity=1)
 
-    else:
-        carts = Cart.objects.filter(
-            session_key=request.session.session_key, product=product)
+        response_data = {
+            "message": "Товар добавлен в корзину",
+            'cart_items_html': self.render_cart(request)
+        }
 
-        if carts.exists():
-            cart = carts.first()
-            if cart:
-                cart.quantity += 1
-                cart.save()
-        else:
-            Cart.objects.create(
-                session_key=request.session.session_key, product=product, quantity=1)
-
-    user_cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
-
-    response_data = {
-        "message": "Товар добавлен в корзину",
-        "cart_items_html": cart_items_html,
-    }
-
-    return JsonResponse(response_data)
+        return JsonResponse(response_data)
 
 
-def cart_change(request):
-    cart_id = request.POST.get("cart_id")
-    quantity = request.POST.get("quantity")
+# def cart_add(request):
+#     product_id = request.POST.get("product_id")
+#
+#     product = Products.objects.get(id=product_id)
+#
+#     if request.user.is_authenticated:
+#         carts = Cart.objects.filter(user=request.user, product=product)
+#
+#         if carts.exists():
+#             cart = carts.first()
+#             if cart:
+#                 cart.quantity += 1
+#                 cart.save()
+#         else:
+#             Cart.objects.create(user=request.user, product=product, quantity=1)
+#
+#     else:
+#         carts = Cart.objects.filter(
+#             session_key=request.session.session_key, product=product)
+#
+#         if carts.exists():
+#             cart = carts.first()
+#             if cart:
+#                 cart.quantity += 1
+#                 cart.save()
+#         else:
+#             Cart.objects.create(
+#                 session_key=request.session.session_key, product=product, quantity=1)
+#
+#     user_cart = get_user_carts(request)
+#     cart_items_html = render_to_string(
+#         "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+#
+#     response_data = {
+#         "message": "Товар добавлен в корзину",
+#         "cart_items_html": cart_items_html,
+#     }
+#
+#     return JsonResponse(response_data)
 
-    cart = Cart.objects.get(id=cart_id)
 
-    cart.quantity = quantity
-    cart.save()
-    updated_quantity = cart.quantity
+class CartChangeView(CartMixin, View):
 
-    cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+    def post(self, request):
+        cart_id = request.POST.get('cart_id')
 
-    response_data = {
-        "message": "Количество изменено",
-        "cart_items_html": cart_items_html,
-        "quaantity": updated_quantity,
-    }
+        cart = self.get_cart(request, cart_id=cart_id)
 
-    return JsonResponse(response_data)
+        cart.quantity = request.POST.get('quantity')
+        cart.save()
+
+        quantity = cart.quantity
+
+        response_data = {
+            'message': 'Колличевство изменено',
+            'quantity': quantity,
+            'cart_items_html': self.render_cart(request)
+        }
+
+        return JsonResponse(response_data)
 
 
-def cart_remove(request):
-    cart_id = request.POST.get("cart_id")
+# def cart_change(request):
+#     cart_id = request.POST.get("cart_id")
+#     quantity = request.POST.get("quantity")
+#
+#     cart = Cart.objects.get(id=cart_id)
+#
+#     cart.quantity = quantity
+#     cart.save()
+#     updated_quantity = cart.quantity
+#
+#     cart = get_user_carts(request)
+#     cart_items_html = render_to_string(
+#         "carts/includes/included_cart.html", {"carts": cart}, request=request)
+#
+#     response_data = {
+#         "message": "Количество изменено",
+#         "cart_items_html": cart_items_html,
+#         "quaantity": updated_quantity,
+#     }
+#
+#     return JsonResponse(response_data)
+#
+#
 
-    cart = Cart.objects.get(id=cart_id)
-    quantity = cart.quantity
-    cart.delete()
 
-    user_cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+class CartRemoveView(CartMixin, View):
 
-    response_data = {
-        "message": "Товар удален",
-        "cart_items_html": cart_items_html,
-        "quantity_deleted": quantity,
-    }
+    def post(self, request):
+        cart_id = request.POST.get('cart_id')
 
-    return JsonResponse(response_data)
+        cart = self.get_cart(request, cart_id=cart_id)
+        quantity = cart.quantity
+        cart.delete()
+
+        response_data = {
+            'message': 'Товар был удалён из корзины',
+            'quantity_deleted': quantity,
+            'cart_items_html': self.render_cart(request)
+        }
+
+        return JsonResponse(response_data)
+
+# def cart_remove(request):
+#     cart_id = request.POST.get("cart_id")
+#
+#     cart = Cart.objects.get(id=cart_id)
+#     quantity = cart.quantity
+#     cart.delete()
+#
+#     user_cart = get_user_carts(request)
+#     cart_items_html = render_to_string(
+#         "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+#
+#     response_data = {
+#         "message": "Товар удален",
+#         "cart_items_html": cart_items_html,
+#         "quantity_deleted": quantity,
+#     }
+#
+#     return JsonResponse(response_data)
 
 
 #_________________ API __________________
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, UpdateView, TemplateView
+
 from carts.models import Cart
 from orders.models import Order, OrderItem
 from django.core.mail import send_mail
@@ -79,59 +82,121 @@ class UserLoginView(LoginView):
 #     return render(request, 'users/login.html', context)
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
+class UserRegistrationView(CreateView):
+    template_name = 'users/registration.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('users:profile')
+
+    def form_valid(self, form):
+        session_key = self.request.session.session_key
+        user = form.instance
+
+        if user:
             form.save()
+            auth.login(self.request, user)
 
-            session_key = request.session.session_key
+        if session_key:
+            Cart.objects.filter(session_key=session_key).update(user=user)
 
-            user = form.instance
-            auth.login(request, user)
+        messages.success(self.request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
+        return HttpResponseRedirect(self.success_url)
 
-            if session_key:
-                Cart.objects.filter(session_key=session_key).update(user=user)
-            messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
-            return HttpResponseRedirect(reverse('home:index'))
-    else:
-        form = UserRegistrationForm()
-
-    context = {
-        'title': 'Home - Регистрация',
-        'form': form
-    }
-    return render(request, 'users/registration.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Регистрация'
+        return context
 
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Профайл успешно обновлен")
-            return HttpResponseRedirect(reverse('user:profile'))
-    else:
-        form = ProfileForm(instance=request.user)
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#
+#             session_key = request.session.session_key
+#
+#             user = form.instance
+#             auth.login(request, user)
+#
+#             if session_key:
+#                 Cart.objects.filter(session_key=session_key).update(user=user)
+#             messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
+#             return HttpResponseRedirect(reverse('home:index'))
+#     else:
+#         form = UserRegistrationForm()
+#
+#     context = {
+#         'title': 'Home - Регистрация',
+#         'form': form
+#     }
+#     return render(request, 'users/registration.html', context)
 
-    orders = Order.objects.filter(user=request.user).prefetch_related(
-        Prefetch(
-            "orderitem_set",
-            queryset=OrderItem.objects.select_related("product"),
-        )
-    ).order_by("-id")
 
-    context = {
-        'title': 'Home - Кабинет',
-        'form': form,
-        'orders': orders,
-    }
-    return render(request, 'users/profile.html', context)
+class UserProfileView(LoginRequiredMixin, UpdateView):
+    template_name = 'users/profile.html'
+    form_class = ProfileForm
+    success_url = reverse_lazy('user:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        messages.success((self.request, "Профайл успешно обновлён"))
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Произошла ошибка")
+        return super().form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Кабинет'
+        context['orders'] = Order.objects.filter(user=self.request.user).prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        ).order_by("-id")
+        return context
 
 
-def users_cart(request):
-    return render(request, 'users/users_cart.html')
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Профайл успешно обновлен")
+#             return HttpResponseRedirect(reverse('user:profile'))
+#     else:
+#         form = ProfileForm(instance=request.user)
+#
+#     orders = Order.objects.filter(user=request.user).prefetch_related(
+#         Prefetch(
+#             "orderitem_set",
+#             queryset=OrderItem.objects.select_related("product"),
+#         )
+#     ).order_by("-id")
+#
+#     context = {
+#         'title': 'Home - Кабинет',
+#         'form': form,
+#         'orders': orders,
+#     }
+#     return render(request, 'users/profile.html', context)
+
+
+class UserCartView(TemplateView):
+    template_name = 'users/users_cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Корзина'
+        return context
+
+
+# def users_cart(request):
+#     return render(request, 'users/users_cart.html')
 
 
 @login_required
